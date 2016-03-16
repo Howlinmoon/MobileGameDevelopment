d3f3831072b6577dbbc28c75393a3e8dba89cbc9
Jim Veneskey <github@strifecrafter.com>
2016-03-16 05:50:44 -0400

    Experimenting with scaling sprites

--- Game 01/js/main.js
+++ Game 01/js/main.js
@@ -12,10 +12,20 @@ var GameState = {
   //executed after everything is loaded
   create: function() {
       this.background = this.game.add.sprite(0, 0, 'background');
+      this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
       // adjust the anchor point of the chicken to the center of the image
       // (X) 0 is far left, 1 is far right, (Y) 0 is the bottom, and 1 is the top
-      this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
       this.chicken.anchor.setTo(0.5, 0.5);
+
+  
+    // adjusting the scale of the image
+      // 1,1 means scale 1x - or no change
+      // 2,2 means scale 2x
+      // 0.5, 0.5 is scale .5 or 1/2x
+      this.chicken.scale.setTo(2, 1);
+      
+      this.horse = this.game.add.sprite(120, 10, 'horse');
+      this.horse.scale.setTo(0.5, 0.5);
   },
   //this is executed multiple times per second
   update: function() {
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # MobileGameDevelopment
 
-Left off with lesson 02-02, Loading a Sprite
+left off with: Flipping
 
-https://stackskills.com/courses/learn-mobile-game-development-by-building-15-games/lectures/323451
+https://stackskills.com/courses/learn-mobile-game-development-by-building-15-games/lectures/323454
+
 
